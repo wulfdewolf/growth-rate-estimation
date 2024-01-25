8c7f0a019542d7c74fda79bf9b6ca7a05dc8cb85
wulfdewolf <s2408107@ed.ac.uk>
2024-01-25 11:25:12 +0000

    first upload + README

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# growth-rate-estimation
-A simple growth-rate estimation script.
+# Growth Rate Estimation
+The matlab file in this repository contains some functions to estimate growth-rate from the `.txt` file outputted by the OGI-BIO Bioreactor.
+
+# 1. Usage
+The main function `compute_growth_rate` takes as input:
+* the path to the `.txt` file
+* the column name of the OD you want to compute growth rate for:
+  - tends to be `OD_A`, `OD_B`, `OD_C`, etc.
+* the length of the smoothing window:
+  - an integer, e.g. 5 represents 5 values in your time resolution
+* the smoothing method:
+  - "gaussian": gaussian smoothing
+  - "avg": rolling average smoothing
+  - "optimise": try out both and see which yields the highest R2
+* the maximum number of change points:
+  - an integer representing the maximum number of slope change points the function will look for
+  - "optimise": try out a bunch of values and use the on that yields the highest R2 (recommended)
+
+Calling the main function will return estimates of:
+* doubling rate
+* doubling time
+* growth rate  
+  
+A figure representing the estimation will also appear.
+
+# 2. Algorithm
+The algorithm uses the matlab [`ischange`](https://uk.mathworks.com/help/matlab/ref/ischange.html) function to look for changes in linear slope in the smoothed log(OD).  
+It fits a linear line to the points between the first two change points and uses the slope of that line to estimate doubling rate, doubling time, and growth rate.
+OD values smaller than 0.05 are dropped, since this represents the detection threshold of the Bioreactor.
+
+
+# 3. Questions
+Feel free to email me if you have questions: [wolf.de.wulf@ed.ac.uk](mailto:wolf.de.wulf@ed.ac.uk)
---
+++ compute_growth_rate.m
@@ -0,0 +1,145 @@
+% Author: Wolf De Wulf
+% (but mostly co-pilot, this can probably be made much shorter)
+% 
+% Example usage:
+%
+% [doubling_rate, doubling_time, growth_rate] = compute_growth_rate("path/to/data.txt", OD_B, 5, "optimize", "optimize");
+% --> this will optimize the smoothing method and number of slope change points to find the one that yields the highest R2
+%
+% BUT you can also manually specify them, atm only "gaussian" and "avg" are supported for smoothing:
+% [doubling_rate, doubling_time, growth_rate] = compute_growth_rate("path/to/data.txt", OD_B, 5, "gaussian", "optimize");
+% [doubling_rate, doubling_time, growth_rate] = compute_growth_rate("path/to/data.txt", OD_B, 5, "optimize", 2);
+% --> note that you can choose the optimize one and specify the other
+%
+% TODO: implement optmization of smoothing window
+
+function [doubling_rate, doubling_time, growth_rate] = compute_growth_rate(path, OD_var, smoothing_window_length, smoothing, max_num_changes)
+
+% Read data
+[od,time] = read_data(path, OD_var);
+
+% Remove noisy values below reasonable detection threshold
+od = od(od > 0.05);
+time = time(od > 0.05);
+
+% Define search spaces
+if string(smoothing) == "optimize"
+    smoothing_methods = ["gaussian", "avg"];
+else
+    smoothing_methods = [smoothing];
+end
+
+if string(max_num_changes) == "optimize"
+    max_num_changes_values = 2:10;
+else
+    max_num_changes_values = [max_num_changes];
+end
+
+% Search for the best smoothing method and max_num_changes
+best_r2 = -Inf;
+best_smoothing = "";
+best_max_num_changes = 0;
+for i = 1:length(smoothing_methods)
+    for j = 1:length(max_num_changes_values)
+        [~, r2, ~, ~, ~, ~, ~, ~] = compute_growth_rate_single(od, time, smoothing_methods(i), smoothing_window_length, max_num_changes_values(j));
+        if r2 > best_r2
+            best_r2 = r2;
+            best_smoothing = smoothing_methods(i);
+            best_max_num_changes = max_num_changes_values(j);
+        end
+    end
+end
+
+[smooth_od, r2, p, brkpt, od_fit, linear_time, linear_od, TF] = compute_growth_rate_single(od,time, best_smoothing, smoothing_window_length, best_max_num_changes);
+
+% Compute growth rate, doubling rate, and doubling time
+doubling_rate = p(1);
+doubling_time = 60 / doubling_rate;
+growth_rate = (log(2) *60) / doubling_time;
+
+% Plot
+plot_growth_rate(OD_var, brkpt, linear_time, linear_od, od_fit, r2, doubling_rate, doubling_time, growth_rate, time, smooth_od, TF, p, best_max_num_changes, best_smoothing);
+
+end
+
+function [smooth_od, r2, p, brkpt, od_fit, linear_time, linear_od, TF] = compute_growth_rate_single(od, time, smoothing, smoothing_window_length, max_num_changes)
+if smoothing == "gaussian"
+    sigma = smoothing_window_length / 2; % Standard deviation for Gaussian filter
+    filter_size = smoothing_window_length; % Size of the filter
+    x = linspace(-filter_size / 2, filter_size / 2, filter_size);
+    gaussian_filter = exp(-x .^ 2 / (2 * sigma ^ 2));
+    gaussian_filter = gaussian_filter' / sum(gaussian_filter); % Normalize
+    smooth_od = conv(od, gaussian_filter, 'same');
+elseif smoothing == "avg"
+    smooth_od = movmean(od, smoothing_window_length);
+end
+
+% Detect the linear segment in log(y)
+TF = ischange(log(smooth_od), 'linear', 'MaxNumChanges', max_num_changes);
+brkpt = time(TF==1);
+
+% Fit a linear regression to the linear segment
+linear_time = time(time>=brkpt(1) & time<=brkpt(2));
+linear_od = log(smooth_od(time>=brkpt(1) & time<=brkpt(2)));
+p = polyfit(linear_time, linear_od, 1);
+
+% Compute R^2
+od_fit = polyval(p, linear_time);
+residuals = linear_od - od_fit;
+ss_res = sum(residuals.^2);
+ss_tot = sum((linear_od - mean(linear_od)).^2);
+r2 = 1 - ss_res / ss_tot;
+end
+
+function plot_growth_rate(OD_var, brkpt, linear_time, linear_od, od_fit, r2, doubling_rate, doubling_time, growth_rate, time, smooth_od, TF, p, max_num_changes, smoothing)
+figure; % Create a new figure window
+
+% Plot the log od
+plot(time, log(smooth_od));
+title(sprintf('Growth Rate (%s)', OD_var));
+xlabel('Time (hours)'); % Adjusted to reflect the change in time units
+ylabel('log(OD)');
+hold on; % Keep the current plot when adding new plots
+
+% Plot the change points
+plot(brkpt, log(smooth_od(TF==1)), 'ro');
+
+% Plot the linear segment
+plot(linear_time, linear_od, 'r', 'LineWidth', 2);
+
+% Plot the linear regression line
+od_fit = polyval(p, linear_time);
+plot(linear_time, od_fit, 'k', 'LineWidth', 2);
+
+% Display the equation, R-squared value, doubling rate, doubling time, and growth rate in the figure
+str = sprintf('y = %.3fx + %.3f\nR^2 = %.3f\nDoubling Rate = %.3f 1/h\nDoubling Time = %.3f min\nGrowth Rate = %.3f 1/h\nMax num changes = %d\nSmoothing: %s', p(1), p(2), r2, doubling_rate, doubling_time, growth_rate, max_num_changes, smoothing);
+annotation('textbox', [0.78, 0.07, 0.2, 0.2], 'String', str, 'FitBoxToText', 'on');
+
+% Add a legend
+legend('log(y)','Slope Change Points',  'Linear Segment', 'Linear Fit');
+
+% Set x-axis to be in hours and have ticks per hour
+xlim([min(time), max(time)]);
+xticks(min(time):1:max(time)); % Adjusted to reflect the change in time units
+end
+
+function [od, time] = read_data(path, OD_var)
+    % Open the file
+    fileID = fopen(path, 'r');
+
+    % Read the first line and split it into parts
+    firstLine = fgetl(fileID);
+    columns = split(firstLine, ',');
+    columns = columns(1:end-2); % Remove the last two entries
+
+    % Close the file
+    fclose(fileID);
+
+    % Read the data into a table
+    opts = detectImportOptions(path);
+    opts.VariableNames = columns;
+    opts.VariableTypes(1:numel(columns)) = {'double'}; % assuming all columns are numeric
+    data = readtable(path, opts);
+    od = data.(OD_var);
+    time = data.("time");
+end
